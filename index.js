2479bdaae038228a96f74885a3eb4ba4ac7cf877
Amie Foster <amie.n.foster@gmail.com>
2021-08-28 14:36:54 -0600

    final changes adding JS

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const headerOne = document.querySelector('h1')
 
 console.log('h1 log',headerOne)
 
+document.getElementById("header").addEventListener("mouseover", function() {
+    document.getElementById("header").style.backgroundColor = "wheat";
+});
+document.getElementById("header").addEventListener("mouseout", function() {
+    document.getElementById("header").style.backgroundColor = "white";
+});
 
-headerOne.style.color = 'orange';
+document.getElementById('footer').addEventListener("mouseover", function(){
+    document.getElementById("footer").style.backgroundColor = "lightpink";
+})
 
-
-const headerTwo = document.querySelector('h2')
-   
-headerTwo.style.color = 'pink';
+document.getElementById('footer').addEventListener("mouseout", function(){
+    document.getElementById("footer").style.backgroundColor = "hotpink";
+})
